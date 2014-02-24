d1ab8101d30b1e7a6c49c7c7038a6f352203d739
MattiasBeimers <matheiusb@gmail.com>
2014-02-24 15:52:58 +0100

    Decent initial try, but how on earth do I get the x coordinate of a button....
    o_O

--- MattyControls.py
+++ MattyControls.py
@@ -1,22 +1,71 @@
 from tkinter import *
 from tkinter.ttk import *
 
+# The constants
+H_LEFT        = 0
+H_COPY_LEFT   = 1
+H_CENTER      = 2
+H_COPY_RIGHT  = 3
+H_RIGHT       = 4
+V_TOP         = 0
+V_COPY_TOP    = 1
+V_MIDDLE      = 2
+V_COPY_BOTTOM = 3
+V_BOTTOM      = 4
+
+# The class
 class Btn(Button):
     """Matty's button class"""
-    def __init__(self, parent, rest):
+
+    # Init method
+    def __init__(self, parent, **kwargs):
         """Create a button given it's text and it's parent frame."""
-        Button.__init__(self, parent, rest) # HOW TO DO THIS???
-        self["text"] = text
+        Button.__init__(self, parent, **kwargs)
 
-    def getText(self):
-        return self["text"]
-    def setText(self, value):
-        self.configure(text = value)
-    text = property(getText, setText)
+    # Some getters and setters
+    x = property(lambda self: self.winfo_x(), lambda self, val: self.place(x = val))
+    y = property(lambda self: self.winfo_y(), lambda self, val: self.place(y = val))
+    width = property(lambda self: self.winfo_width(), lambda self, val: self.configure(width = val))
+    height = property(lambda self: self.winfo_height(), lambda self, val: self.configure(height = val))
+    text = property(lambda self: self["text"], lambda self, val: self.configure(text = val))
+    command = property(lambda self: self["command"], lambda self, val: self.configure(command = val))
 
-    def getCommand(self):
-        return self["command"]
-    def setCommand(self, value):
-        self.configure(command = value)
-    command = property(getCommand, setCommand)
+    # The positioning methods
+    def locateInside(self, c, h=H_LEFT, v=V_TOP, d=10):
+        """Locate the current control inside c at the horizontal placement h, the vertical placement v and with a margin of d"""
+        x = 0
+        y = 0
+
+        if h == H_LEFT:        x = d
+        #if h == H_COPY_LEFT:   x = c.winfo_x()
+        #if h == H_CENTER:      x = (c.winfo_width() - self.winfo_width()) / 2
+        #if h == H_COPY_RIGHT:  x = c.winfo_x() + c.winfo_width() - self.winfo_width()
+        #if h == H_RIGHT:       x = c.winfo_width() - self.winfo_width() - d
+
+        if v == V_TOP:         y = d
+        #if v == V_COPY_TOP:    y = c.winfo_y()
+        #if v == V_MIDDLE:      y = (c.winfo_height() - self.winfo_height()) / 2
+        #if v == V_COPY_BOTTOM: y = c.winfo_y() + c.winfo_height() - self.winfo_height()
+        #if v == V_BOTTOM:      y = c.winfo_height() - self.winfo_height() - d
+
+        self.place(x=x, y=y)
+
+    def locateFrom(self, c, h=H_LEFT, v=V_TOP, d=10):
+        """Locate the current control relative to c at the horizontal placement h, the vertical placement v and with a margin of d"""
+        x = 0
+        y = 0
+
+        if h == H_LEFT:        x = c.x - self.width - d
+        #if h == H_COPY_LEFT:   x = c.x
+        #if h == H_CENTER:      x = c.x + (c.width - self.width) / 2
+        #if h == H_COPY_RIGHT:  x = c.x + c.width - self.width
+        #if h == H_RIGHT:       x = c.x + c.width + d
+
+        if v == V_TOP:         y = c.y - self.height - d
+        #if v == V_COPY_TOP:    y = c.y
+        #if v == V_MIDDLE:      y = c.y + (c.height - self.height) / 2
+        #if v == V_COPY_BOTTOM: y = c.y + c.height - self.height
+        #if v == V_BOTTOM:      y = c.y + c.height + d
+
+        self.place(x=x, y=y)
 
--- tk_test.py
+++ tk_test.py
@@ -1,34 +1,48 @@
 from tkinter import *
+from tkinter import messagebox
 from tkinter.ttk import *
 #from MattyControls import *
 
 
 class Application(Frame):
     def say_hi(self):
+        """Print Hi to the console"""
         print("Hi there, everyone!")
 
     def createWidgets(self):
+        """Create all the buttons and other widgets"""
         self.master.title("Test")
-        self.pack(fill=BOTH)
+        self.pack(fill=BOTH, expand=1)
 
-        self.btnHi = Button(self, text="Hello", command=self.say_hi)
-        self.btnHi.pack(side=LEFT, padx=10, pady=10)
-        #self.btnHi.place(x=10, y=10)
 
-        self.btnQuit = Button(self, text="Quit", command=self.quit)
-        self.btnQuit.pack(side=LEFT, padx=0, pady=10)
-        #self.btnQuit.place(x=50, y=10)
+        test = Button(self, text="test", command=self.quit)
+        test.place(x=10, y=11)
+        messagebox.showinfo("msg", test.winfo_width)
+        self.quit()
 
-        self.btnTest = Button(self, text="Test", command=self.quit)
-        self.btnTest.pack(side=TOP, padx=0, pady=10)
+        #self.btnHi = Btn(self)
+        #self.btnHi.text = "Hello"
+        #self.btnHi.command = self.say_hi
+        #self.btnHi.locateInside(self, H_LEFT, V_TOP, 10)
+
+        #self.btnQuit = Btn(self, text="Quit", command=self.quit)
+        #self.btnQuit.locateFrom(self.btnHi, H_LEFT, V_COPY_TOP, 10)
+
+        #self.btnTest = Btn(self, text="Test - quit", command=self.quit)
+        #self.btnTest.locateFrom(self.btnHi, H_COPY_LEFT, V_BOTTOM, 10)
 
     def __init__(self, master=None):
+        """The constructor"""
         Frame.__init__(self, master)
         self.createWidgets()
 
 
-root = Tk()
-root.geometry("350x200")
-app = Application(master=root)
-app.mainloop()
+def main():
+    """The main entrypoint for this application"""
+    root = Tk()
+    root.geometry("350x200")
+    app = Application(master=root)
+    app.mainloop()
 
+if __name__ == '__main__':
+    main()

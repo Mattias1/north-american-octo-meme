079530178a57aba96dbb38683c698acf4971bed2
Chiel92 <ctje92@gmail.com>
2014-03-26 17:11:14 +0100

    more asdf

--- run.py
+++ run.py
@@ -7,11 +7,12 @@ import scipy.stats
 start_time = time()
 print('start_time: ' + str(start_time))
 
-seeds = [470, 770, 689, 376, 100, 753, 755, 682, 243, 432,
-         454, 760, 947, 184, 299, 631, 571, 644, 650, 483]
-duration = 24 * 60 * 60
+#seeds = [204, 555, 359, 923, 172, 413, 498, 849, 130, 702, 985, 743, 525, 288, 965, 222, 601, 572, 807, 321, 248, 223, 287, 669, 252, 119, 417, 763, 307, 349, 361, 740, 449, 153, 894, 551, 749, 730, 857, 910, 680, 343, 820, 792, 937, 443, 160, 324, 833, 752, 193, 766, 173, 465, 669, 451, 216, 950, 956, 511, 367, 315, 738, 179, 355, 591, 428, 294, 550, 170, 522, 232, 306, 350, 401, 508, 429, 620, 862, 251, 482, 975, 337, 843, 908, 901, 375, 499, 184, 827, 305, 675, 407, 945, 410, 368, 958, 308, 876, 229]
+seeds = [470, 770, 689, 376, 100, 753, 755, 682, 243, 432, 454, 760, 947, 184, 299, 631, 571, 644, 650, 483]
+duration = 2 * 24 * 60 * 60
 
-configuration_space = [15, 20, 40]
+configuration_space = [20, 40, 200]
+configuration_space = [15]
 
 
 class Sample:
@@ -32,7 +33,7 @@ class Configuration:
         self.samples = []
 
         for i, seed in enumerate(seeds):
-            print('Running {}th simulation at {}'.format(i, str(time() - start_time)))
+            # print('Running {}th simulation at {}'.format(i, str(time() - start_time)))
             factory = Factory(11, 8, self.batchsize, self.buffersizeA,
                               self.buffersizeB, self.buffersizeC, seed, duration, silent=True)
             factory.play()
@@ -48,16 +49,18 @@ class Configuration:
             self.samples.append(sample)
 
         self.mean_produced, self.deviation_produced, *self.confidence_interval_produced = (
-            mean_confidence_interval(s.total_produced for s in self.samples)
+            self.mean_confidence_interval([float(s.total_produced) for s in self.samples])
         )
 
         self.mean_throughput, self.deviation_throughput, *self.confidence_interval_throughput = (
-            mean_confidence_interval(s.total_throughput for s in self.samples)
+            self.mean_confidence_interval([float(s.average_throughput) for s in self.samples])
         )
 
     def __str__(self):
-        return str(self.mean_produced, self.deviation_produced, self.confidence_interval_produced,
-                   self.mean_throughput, self.deviation_throughput, self.confidence_interval_throughput)
+        return 'configuration: {}\nresult: {}\n'.format(
+                str((self.batchsize, self.buffersizeA, self.buffersizeB, self.buffersizeC)),
+                str((self.mean_produced, self.deviation_produced, self.confidence_interval_produced,
+                   self.mean_throughput, self.deviation_throughput, self.confidence_interval_throughput)))
 
     def __lt__(self, conf2):
         return self.mean_produced < conf2.mean_produced
@@ -66,6 +69,7 @@ class Configuration:
     def mean_confidence_interval(data, confidence=0.95):
         a = 1.0 * array(data)
         n = len(a)
+        assert n > 1
         m, s = mean(a), scipy.stats.sem(a)
         h = s * scipy.stats.t._ppf((1 + confidence) / 2., n - 1)
         return m, s, m - h, m + h
@@ -73,7 +77,7 @@ class Configuration:
 
 def run():
     results = []
-    for batchsize in configuration_space:
+    for batchsize in [10, 20, 40]:
         for buffersizeA in configuration_space:
             for buffersizeB in configuration_space:
                 for buffersizeC in configuration_space:
@@ -81,6 +85,6 @@ def run():
                         continue
                     results.append(Configuration(batchsize, buffersizeA, buffersizeB, buffersizeC))
 
-    print('\n'.join(sorted(results)))
+    print('\n'.join((str(r) for r in sorted(results))))
 
 run()
